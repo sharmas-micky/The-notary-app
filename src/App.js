a0221bc4cab7ac86b6705a29ce92f1e9d04bf414
sharmas-micky <82169761+sharmas-micky@users.noreply.github.com>
2022-09-20 13:03:37 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  
+  const [Data , setData] = useState({})
+
+  const fetchdata = async ()=>{
+     await axios.post('https://demo2211087.mockable.io/mock')
+    .then(response =>{
+      var parsedData = response.data
+      setData(parsedData)
+    })
+  }
+  
+  useEffect( ()=>{
+    fetchdata();
+    // eslint-disable-next-line
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Welcome To The Notary App</h1>
+    <div className="App App-header">
+            <table className="table table-primary">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Data.companies && Data.companies.map((element)=>{
+                 return(
+                  <tr>
+                      <td >{element.name}</td>
+                      <td >{element.email}</td>
+                      <td >{element.status}</td>
+                </tr>
+                 )
+               })}   
+
+        </tbody>
+      </table>
+
     </div>
+    </>
   );
 }
 
